823124187846d91cb003481e933cd16932beff52
walter <walter.d.retzer@gmail.com>
2024-05-21 17:24:37 -0300

    Incluído efeito de animação no logo da Splash Screen.

--- composeApp/src/commonMain/kotlin/screens/SplashScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/SplashScreen.kt
@@ -1,26 +1,38 @@
 package screens
 
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import halfmouthappproject.composeapp.generated.resources.Res
 import halfmouthappproject.composeapp.generated.resources.logohalfmouth
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
+
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 @Preview
 fun SplashScreen() {
-
     val scale = remember { Animatable(0f) }
-
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 1f,
+            animationSpec = spring(
+                dampingRatio = Spring.DampingRatioHighBouncy,
+                stiffness = Spring.StiffnessMedium
+            )
+        )
+    }
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
@@ -28,7 +40,7 @@ fun SplashScreen() {
         Image(
             painter = painterResource(Res.drawable.logohalfmouth),
             contentDescription = "Logo",
-            //modifier = Modifier.scale(scale.value)
+            modifier = Modifier.scale(scale.value)
         )
     }
 }
