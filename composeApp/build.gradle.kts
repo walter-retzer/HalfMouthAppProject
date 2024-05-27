23a51816752278642a2eb9f70130e31831adffe6
walter <walter.d.retzer@gmail.com>
2024-05-27 14:55:38 -0300

    Import da biblioteca do Google Services e do Firebase Auth da GitLive para autenticação e cadastro de novos usuários utilizando o Firebase.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.googleServices)
 }
 
 kotlin {
@@ -40,6 +41,7 @@ kotlin {
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(compose.animation)
+            implementation(libs.firebase.auth)
         }
     }
 }
