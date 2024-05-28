691228602d0bbf1ab43d31a4a48fec51976164dc
walter <walter.d.retzer@gmail.com>
2024-05-28 16:46:13 -0300

    Adicionado Class ConstantsApp

---
+++ composeApp/src/commonMain/kotlin/util/ConstantsApp.kt
@@ -0,0 +1,9 @@
+package util
+
+class ConstantsApp {
+        companion object {
+            val PASSWORD_MAX_NUMBER = 6
+            val PHONE_MAX_NUMBER = 11
+            val NAME_MAX_NUMBER = 25
+        }
+}
