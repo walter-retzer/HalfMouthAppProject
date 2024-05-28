cb1d169c95f148bb7b368a2f4a6c4752d11296d2
walter <walter.d.retzer@gmail.com>
2024-05-28 18:19:37 -0300

    Incluido na SignInViewModel fun para validação dos campos de nome, celular, email e senha.

--- composeApp/src/commonMain/kotlin/viewmodel/SignInViewModel.kt
+++ composeApp/src/commonMain/kotlin/viewmodel/SignInViewModel.kt
@@ -1,84 +1,87 @@
 package viewmodel
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import dev.icerock.moko.mvvm.viewmodel.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import util.ConstantsApp
 
 
 class SignInViewModel : ViewModel() {
 
-    var newUser: NewUserContact by mutableStateOf(
-        NewUserContact(
-            name = "",
-            phoneNumber = "",
-            email = "",
-            password = "",
-            confirmPassword = ""
-        )
-    )
-        private set
+    private val _uiState = MutableStateFlow(NewUserContact())
+    val uiState = _uiState.asStateFlow()
+
+    private val _emailError = MutableStateFlow(false)
+    val emailError = _emailError.asStateFlow()
+
+    private val _passwordError = MutableStateFlow(false)
+    val passwordError = _passwordError.asStateFlow()
+
+    private val _nameError = MutableStateFlow(false)
+    val nameError = _nameError.asStateFlow()
+
+    private val _phoneNumberError = MutableStateFlow(false)
+    val phoneNumberError = _phoneNumberError.asStateFlow()
 
 
+    fun onEmailChange(newValue: String) {
+        _uiState.update { it.copy(email = newValue) }
+        //reset error when the user types another character
+        if (newValue.isNotBlank()) _emailError.value = false
+    }
 
-    fun onEvent(event: SignInContactEvent) {
-        when (event) {
-            is SignInContactEvent.OnFirstNameChanged -> {
-                newUser = newUser.copy(
-                    name = event.value
-                )
-            }
+    fun onPasswordChange(newValue: String) {
+        _uiState.update { it.copy(password = newValue) }
+        //reset error when the user types another character
+        if (newValue.isNotBlank()) _passwordError.value = false
+    }
 
-            is SignInContactEvent.OnPhoneNumberChanged -> {
-                newUser = newUser.copy(
-                    phoneNumber = event.value
-                )
-            }
+    fun onNameChange(newValue: String) {
+        _uiState.update { it.copy(name = newValue) }
+        //reset error when the user types another character
+        if (newValue.isNotBlank()) _nameError.value = false
+    }
 
-            is SignInContactEvent.OnEmailChanged -> {
-                newUser = newUser.copy(
-                    email = event.value
-                )
-            }
+    fun onPhoneNumberChange(newValue: String) {
+        _uiState.update { it.copy(phoneNumber = newValue) }
+        //reset error when the user types another character
+        if (newValue.isNotBlank()) _phoneNumberError.value = false
+    }
 
-            is SignInContactEvent.OnPasswordChanged -> {
-                newUser = newUser.copy(
-                    password = event.value
-                )
-            }
-
-            is SignInContactEvent.OnConfirmPasswordChanged -> {
-                newUser = newUser.copy(
-                    confirmPassword = event.value
-                )
-            }
-
-            SignInContactEvent.SaveContact -> TODO()
+    fun validateEmail(email: String): String {
+        val emailRegex = Regex("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}\$")
+        if(!emailRegex.matches(email)) {
+            _emailError.value = true
         }
+        return "Verifique o email digitado"
+    }
+
+    fun validatePassword(password: String): String {
+        if(password.length != ConstantsApp.PASSWORD_MAX_NUMBER) {
+            _passwordError.value = true
+        }
+        return "A senha deve conter 6 dígitos"
+    }
+
+    fun validatePhoneNumber(phoneNumber: String): String {
+        if(phoneNumber.length != ConstantsApp.PHONE_MAX_NUMBER) {
+            _phoneNumberError.value = true
+        }
+        return "Verifique o número digitado"
+    }
+
+    fun validateName(name: String): String {
+        if(name.isEmpty()) {
+            _nameError.value = true
+        }
+        return "Verifique o nome digitado"
     }
 }
 
-        sealed interface SignInContactEvent {
-            data class OnFirstNameChanged(val value: String): SignInContactEvent
-            data class OnPasswordChanged(val value: String): SignInContactEvent
-            data class OnConfirmPasswordChanged(val value: String): SignInContactEvent
-            data class OnEmailChanged(val value: String): SignInContactEvent
-            data class OnPhoneNumberChanged(val value: String): SignInContactEvent
-            object SaveContact: SignInContactEvent
-        }
-
 data class NewUserContact(
-    val name: String,
-    val phoneNumber: String,
-    val email: String,
-    val password: String,
-    val confirmPassword: String,
-)
-
-data class SignInContactErrorState(
-    val nameError: String? = null,
-    val phoneNumberError: String? = null,
-    val emailError: String? = null,
-    val passwordError: String? = null,
-    val confirmPasswordError: String? = null,
+    val name: String = "",
+    val phoneNumber: String = "",
+    val email: String = "",
+    val password: String = "",
 )
