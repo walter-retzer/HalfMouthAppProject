5dd935338484f02f144e64e937ce7dc6945a07fe
walter <walter.d.retzer@gmail.com>
2024-05-28 11:53:45 -0300

    Ajuste de cor do progressBar.

--- composeApp/src/commonMain/kotlin/components/CircularProgressBar.kt
+++ composeApp/src/commonMain/kotlin/components/CircularProgressBar.kt
@@ -1,14 +1,14 @@
 package components
 
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import theme.mainYellowColor
 
 @Composable
 fun MyAppCircularProgressIndicator(modifier: Modifier = Modifier) {
     CircularProgressIndicator(
         modifier = modifier,
-        color = MaterialTheme.colorScheme.secondary
+        color = mainYellowColor
     )
 }
