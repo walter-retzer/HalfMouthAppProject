0fc248684693967d9e6ba7fc6d5328c680f34705
walter <walter.d.retzer@gmail.com>
2024-05-28 16:47:36 -0300

    Adicionados fun de validação dos campos de email e senha.

---
+++ composeApp/src/commonMain/kotlin/viewmodel/LoginUserViewModel.kt
@@ -0,0 +1,52 @@
+package viewmodel
+
+import dev.icerock.moko.mvvm.viewmodel.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import util.ConstantsApp
+
+class LoginUserViewModel : ViewModel(){
+
+    private val _emailError = MutableStateFlow(false)
+    val emailError = _emailError.asStateFlow()
+
+    private val _passwordError = MutableStateFlow(false)
+    val passwordError = _passwordError.asStateFlow()
+
+    private val _uiState = MutableStateFlow(LoginUserData())
+    val uiState = _uiState.asStateFlow()
+
+    fun onEmailChange(newValue: String) {
+        _uiState.update { it.copy(email = newValue) }
+        //reset error when the user types another character
+        if (newValue.isNotBlank()) _emailError.value = false
+    }
+
+    fun onPasswordChange(newValue: String) {
+        _uiState.update { it.copy(password = newValue) }
+        //reset error when the user types another character
+        if (newValue.isNotBlank()) _passwordError.value = false
+    }
+
+    fun validateEmail(email: String): String {
+        val emailRegex = Regex("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}\$")
+        if(!emailRegex.matches(email)) {
+            _emailError.value = true
+        }
+        return "Verifique o email digitado"
+    }
+
+    fun validatePassword(password: String): String {
+        if(password.length <= ConstantsApp.PASSWORD_MAX_NUMBER) {
+            _passwordError.value = true
+        }
+        return "A senha deve conter 6 dígitos"
+    }
+}
+
+data class LoginUserData(
+    val email: String = "",
+    val password: String = "",
+)
+
