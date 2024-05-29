3f6cafcd61b1d16434eb29ba4b6786f8ebfe8f70
walter <walter.d.retzer@gmail.com>
2024-05-29 11:25:53 -0300

    Ajustado exibição do logo da SplashScreen.kt

--- composeApp/src/commonMain/kotlin/screens/SplashScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/SplashScreen.kt
@@ -6,12 +6,17 @@ import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import halfmouthappproject.composeapp.generated.resources.Res
 import halfmouthappproject.composeapp.generated.resources.logohalfmouth
 import org.jetbrains.compose.resources.ExperimentalResourceApi
@@ -39,8 +44,12 @@ fun SplashScreen() {
     ) {
         Image(
             painter = painterResource(Res.drawable.logohalfmouth),
-            contentDescription = "Logo",
-            modifier = Modifier.scale(scale.value)
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier
+                .scale(scale.value)
+                .size(200.dp)
+                .clip(RoundedCornerShape(50.dp))
         )
     }
 }
