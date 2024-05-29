93af7b8c575e6788b1d3862f4d86b844321ef360
walter <walter.d.retzer@gmail.com>
2024-05-29 11:24:47 -0300

    Ajustado estilo do texto do ProgressButton.kt

--- composeApp/src/commonMain/kotlin/components/ProgressButton.kt
+++ composeApp/src/commonMain/kotlin/components/ProgressButton.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,7 +13,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import theme.darkScheme
 import theme.mainYellowColor
 
@@ -52,7 +55,13 @@ fun ProgressButton(
             Text(
                 text = text,
                 color = darkScheme.onPrimary,
-                style = MaterialTheme.typography.titleMedium
+                style = TextStyle(
+                    fontFamily = FontFamily.SansSerif,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 17.sp,
+                    lineHeight = 16.sp,
+                    letterSpacing = 0.5.sp
+                )
             )
         }
     }
