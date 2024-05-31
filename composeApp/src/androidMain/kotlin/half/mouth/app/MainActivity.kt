15a8efa89e3f298686944907cfbbf05fe01ce42a
walter <walter.d.retzer@gmail.com>
2024-05-30 21:39:59 -0300

    Passado o parametro rootComponent para a fun App.

--- composeApp/src/androidMain/kotlin/half/mouth/app/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/half/mouth/app/MainActivity.kt
@@ -6,31 +6,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
+import com.arkivanov.decompose.ExperimentalDecomposeApi
+import com.arkivanov.decompose.retainedComponent
+import navigation.RootComponentNavigation
 
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalDecomposeApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         WindowCompat.setDecorFitsSystemWindows(window, true)
         window.statusBarColor= Color.BLACK
         WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightStatusBars = false
+
+        val root = retainedComponent { RootComponentNavigation(it) }
+
         setContent {
             App(
                 darkTheme = isSystemInDarkTheme(),
                 dynamicColor = false,
+                rootComponent = root
             )
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App(
-        darkTheme = isSystemInDarkTheme(),
-        dynamicColor = true,
-    )
 }
