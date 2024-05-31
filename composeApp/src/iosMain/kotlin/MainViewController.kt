b3482b13b3c197ead9219cceb591b3c5dd09ec42
walter <walter.d.retzer@gmail.com>
2024-05-30 21:41:47 -0300

    Passado o parametro rootComponent para a fun App no iOS.

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,4 +1,8 @@
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import navigation.RootComponentNavigation
 import platform.UIKit.UIScreen
 import platform.UIKit.UIUserInterfaceStyle
 
@@ -7,8 +11,12 @@ fun MainViewController() = ComposeUIViewController {
     val isDarkTheme =
         UIScreen.mainScreen.traitCollection.userInterfaceStyle ==
                 UIUserInterfaceStyle.UIUserInterfaceStyleDark
+
+    val root = remember{ RootComponentNavigation(DefaultComponentContext(LifecycleRegistry()))}
+
     App(
         darkTheme = isDarkTheme,
         dynamicColor = false,
+        rootComponent = root
     )
 }
