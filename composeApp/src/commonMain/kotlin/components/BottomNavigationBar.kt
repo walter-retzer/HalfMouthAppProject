73ca5dc36640084b3910a38fbd83ae828d6ddf8b
walter <walter.d.retzer@gmail.com>
2024-06-06 12:28:16 -0300

    Criado component BottomNavigationBar

---
+++ composeApp/src/commonMain/kotlin/components/BottomNavigationBar.kt
@@ -0,0 +1,44 @@
+package components
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+import navigation.home.NavItem
+
+@Composable
+fun BottomNavigationBar(navController: NavHostController) {
+    val navItems = listOf(NavItem.Home, NavItem.Search, NavItem.List, NavItem.Profile)
+    var selectedItem by rememberSaveable { mutableStateOf(0) }
+
+    NavigationBar(
+        containerColor = Color.Black,
+        contentColor = Color.Black
+    ) {
+        navItems.forEachIndexed { index, item ->
+            NavigationBarItem(
+                alwaysShowLabel = true,
+                icon = { Icon(item.icon, contentDescription = item.title) },
+                label = { Text(item.title) },
+                selected = selectedItem == index,
+                onClick = {
+                    selectedItem = index
+                    navController.navigate(item.path) {
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) { saveState = true }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
