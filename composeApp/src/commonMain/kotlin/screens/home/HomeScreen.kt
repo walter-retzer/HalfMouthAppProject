0433f7cceff12b297ab33ad20890c7389c4f19ee
walter <walter.d.retzer@gmail.com>
2024-06-06 12:30:26 -0300

    Ajustado fun da HomeScreen com os imports dos componentes AppToolbarLarge e BottomNavigationBar

--- composeApp/src/commonMain/kotlin/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/home/HomeScreen.kt
@@ -6,35 +6,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.LargeTopAppBar
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import navigation.home.NavItem
+import components.AppToolbarLarge
+import components.BottomNavigationBar
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import theme.primaryDark
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -46,42 +32,20 @@ fun MainMenuScreen() {
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            LargeTopAppBar(
-                colors = TopAppBarDefaults.largeTopAppBarColors(
-                    containerColor = Color.Black,
-                    titleContentColor = primaryDark,
-                    scrolledContainerColor = Color.Black
-                ),
-                title = {
-                    Text("Top app bar")
-                },
-                navigationIcon = {
-                    IconButton(onClick = { /* do something */ }) {
-                        Icon(
-                            imageVector = Icons.Filled.Menu,
-                            contentDescription = "Localized description"
-                        )
-                    }
-                },
-                actions = {
-                    IconButton(onClick = { /* do something */ }) {
-                        Icon(
-                            imageVector = Icons.Filled.AccountCircle,
-                            contentDescription = "Localized description"
-                        )
-                    }
-                    IconButton(onClick = { /* do something */ }) {
-                        Icon(
-                            imageVector = Icons.Default.Settings,
-                            contentDescription = "Localized description"
-                        )
-                    }
-                },
+            AppToolbarLarge(
+                title = "Menu Pricipal",
+                onNavigationLeftIconClick = { },
+                onNavigationProfileIconClick = { },
+                onNavigationSettingsIconClick = { },
                 scrollBehavior = scrollBehavior
             )
         },
         bottomBar = {
-            BottomAppBar(contentColor = Color.Black, containerColor = Color.Black) {
+            BottomAppBar(
+                contentColor = Color.Black,
+                containerColor = Color.Black
+            )
+            {
                 BottomNavigationBar(navController = rememberNavController())
             }
         }
@@ -139,36 +103,5 @@ fun SubItemTitle() {
                     You have pressed the floating action button times.
                 """.trimIndent(),
         )
-    }
-}
-
-
-@Composable
-fun BottomNavigationBar(navController: NavHostController) {
-    val navItems = listOf(NavItem.Home, NavItem.Search, NavItem.List, NavItem.Profile)
-    var selectedItem by rememberSaveable { mutableStateOf(0) }
-
-    NavigationBar(
-        containerColor = Color.Black,
-        contentColor = Color.Black
-    ) {
-        navItems.forEachIndexed { index, item ->
-            NavigationBarItem(
-                alwaysShowLabel = true,
-                icon = { Icon(item.icon, contentDescription = item.title) },
-                label = { Text(item.title) },
-                selected = selectedItem == index,
-                onClick = {
-                    selectedItem = index
-                    navController.navigate(item.path) {
-                        navController.graph.startDestinationRoute?.let { route ->
-                            popUpTo(route) { saveState = true }
-                        }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
-                }
-            )
-        }
     }
 }
