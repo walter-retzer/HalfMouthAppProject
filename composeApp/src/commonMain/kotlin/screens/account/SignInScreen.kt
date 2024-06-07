7247f27f1bb69cd616265e624dc758909d54d555
walter <walter.d.retzer@gmail.com>
2024-06-07 18:14:43 -0300

    Implementado val settings para salvar o uid do usuario e inclusão de snackbar para o evento de cadastro de usuário no Firebase.

--- composeApp/src/commonMain/kotlin/screens/account/SignInScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/account/SignInScreen.kt
@@ -20,8 +20,12 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -31,13 +35,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import com.russhwolf.settings.Settings
 import components.ProgressButton
+import data.UserPreferences
 import dev.gitlive.firebase.Firebase
 import dev.gitlive.firebase.auth.FirebaseUser
 import dev.gitlive.firebase.auth.auth
@@ -45,12 +52,14 @@ import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
 import halfmouthappproject.composeapp.generated.resources.Res
 import halfmouthappproject.composeapp.generated.resources.splashscreenlogo
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import util.ConstantsApp
 import util.MaskVisualTransformation
+import util.snackBarOnlyMessage
 import viewmodel.SignInViewModel
 
 @OptIn(ExperimentalResourceApi::class)
@@ -58,6 +67,7 @@ import viewmodel.SignInViewModel
 @Preview
 fun SignInScreen(
     modifier: Modifier = Modifier,
+    onNavigateToHome: () -> Unit,
 ) {
     val viewModel = getViewModel(
         key = "sign-in",
@@ -72,166 +82,196 @@ fun SignInScreen(
     val passwordError by viewModel.passwordError.collectAsState()
     val scope = rememberCoroutineScope()
     val auth = remember { Firebase.auth }
+    val snackBarHostState = remember { SnackbarHostState() }
+    val settings = Settings()
     var firebaseUser: FirebaseUser? by remember { mutableStateOf(null) }
     var progressButtonIsActivated by remember { mutableStateOf(false) }
 
-    BoxWithConstraints(
-        modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackBarHostState) }
     ) {
-        val width = this.maxWidth
-        val finalModifier = if (width >= 780.dp) modifier.width(400.dp) else modifier.fillMaxWidth()
-        Column(
-            modifier = finalModifier.padding(start = 16.dp, end = 16.dp).fillMaxHeight()
-                .verticalScroll(rememberScrollState()),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
+        BoxWithConstraints(
+            modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center
         ) {
+            val width = this.maxWidth
+            val finalModifier =
+                if (width >= 780.dp) modifier.width(400.dp) else modifier.fillMaxWidth()
+            Column(
+                modifier = finalModifier.padding(start = 16.dp, end = 16.dp).fillMaxHeight()
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
 
-            Spacer(modifier = Modifier.height(16.dp))
+                Spacer(modifier = Modifier.height(16.dp))
 
-            Text(
-                text = "Criar uma conta",
-                style = MaterialTheme.typography.titleLarge,
-                color = MaterialTheme.colorScheme.secondary
-            )
+                Text(
+                    text = "Criar uma conta",
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.secondary
+                )
 
-            Spacer(modifier = Modifier.height(16.dp))
+                Spacer(modifier = Modifier.height(16.dp))
 
-            Image(
-                painter = painterResource(Res.drawable.splashscreenlogo),
-                contentDescription = null,
-                contentScale = ContentScale.Inside,
-                modifier = Modifier
-                    .size(180.dp)
-                    .clip(CircleShape)
-                    .border(2.dp, androidx.compose.ui.graphics.Color.White, CircleShape)
-                    .background(androidx.compose.ui.graphics.Color.White)
-            )
+                Image(
+                    painter = painterResource(Res.drawable.splashscreenlogo),
+                    contentDescription = null,
+                    contentScale = ContentScale.Inside,
+                    modifier = Modifier
+                        .size(180.dp)
+                        .clip(CircleShape)
+                        .border(2.dp, Color.White, CircleShape)
+                        .background(Color.White)
+                )
 
-            Spacer(modifier = Modifier.height(16.dp))
+                Spacer(modifier = Modifier.height(16.dp))
 
-            Text(
-                modifier = Modifier.align(alignment = Alignment.Start),
-                text = "Inscreva-se para começar:",
-                style = MaterialTheme.typography.bodyLarge,
-                color = MaterialTheme.colorScheme.secondary,
-            )
+                Text(
+                    modifier = Modifier.align(alignment = Alignment.Start),
+                    text = "Inscreva-se para começar:",
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.secondary,
+                )
 
-            Spacer(modifier = Modifier.height(10.dp))
+                Spacer(modifier = Modifier.height(10.dp))
 
-            OutlinedTextField(
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions(
-                    capitalization = KeyboardCapitalization.Words,
-                    autoCorrect = true,
-                    keyboardType = KeyboardType.Text,
-                    imeAction = ImeAction.Next
-                ),
-                shape = RoundedCornerShape(20.dp),
-                value = uiState.name,
-                isError = nameError,
-                supportingText = {
-                    if (nameError) Text(text = viewModel.validateName(uiState.name))
-                },
-                placeholder = { Text("Nome") },
-                onValueChange = {
-                    if (it.length <= ConstantsApp.NAME_MAX_NUMBER) viewModel.onNameChange(it)
-                }
-            )
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(
+                        capitalization = KeyboardCapitalization.Words,
+                        autoCorrect = true,
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Next
+                    ),
+                    shape = RoundedCornerShape(20.dp),
+                    value = uiState.name,
+                    isError = nameError,
+                    supportingText = {
+                        if (nameError) Text(text = viewModel.validateName(uiState.name))
+                    },
+                    placeholder = { Text("Nome") },
+                    onValueChange = {
+                        if (it.length <= ConstantsApp.NAME_MAX_NUMBER) viewModel.onNameChange(it)
+                    }
+                )
 
-            Spacer(modifier = Modifier.height(6.dp))
+                Spacer(modifier = Modifier.height(6.dp))
 
-            OutlinedTextField(
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions(
-                    capitalization = KeyboardCapitalization.None,
-                    autoCorrect = true,
-                    keyboardType = KeyboardType.Phone,
-                    imeAction = ImeAction.Next
-                ),
-                shape = RoundedCornerShape(20.dp),
-                value = uiState.phoneNumber,
-                isError = phoneError,
-                supportingText = {
-                    if (phoneError) Text(text = viewModel.validatePhoneNumber(uiState.phoneNumber))
-                },
-                placeholder = { Text("Celular") },
-                onValueChange = {
-                    if (it.length <= ConstantsApp.PHONE_MAX_NUMBER) viewModel.onPhoneNumberChange(it)
-                },
-                visualTransformation = MaskVisualTransformation(MaskVisualTransformation.PHONE)
-            )
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(
+                        capitalization = KeyboardCapitalization.None,
+                        autoCorrect = true,
+                        keyboardType = KeyboardType.Phone,
+                        imeAction = ImeAction.Next
+                    ),
+                    shape = RoundedCornerShape(20.dp),
+                    value = uiState.phoneNumber,
+                    isError = phoneError,
+                    supportingText = {
+                        if (phoneError) Text(text = viewModel.validatePhoneNumber(uiState.phoneNumber))
+                    },
+                    placeholder = { Text("Celular") },
+                    onValueChange = {
+                        if (it.length <= ConstantsApp.PHONE_MAX_NUMBER) viewModel.onPhoneNumberChange(
+                            it
+                        )
+                    },
+                    visualTransformation = MaskVisualTransformation(MaskVisualTransformation.PHONE)
+                )
 
-            Spacer(modifier = Modifier.height(6.dp))
+                Spacer(modifier = Modifier.height(6.dp))
 
-            OutlinedTextField(
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions(
-                    capitalization = KeyboardCapitalization.None,
-                    autoCorrect = true,
-                    keyboardType = KeyboardType.Email,
-                    imeAction = ImeAction.Next
-                ),
-                shape = RoundedCornerShape(20.dp),
-                value = uiState.email,
-                isError = emailError,
-                supportingText = {
-                    if (emailError) Text(text = viewModel.validateEmail(uiState.email))
-                },
-                placeholder = { Text(text = "Email") },
-                onValueChange = { viewModel.onEmailChange(it) },
-            )
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(
+                        capitalization = KeyboardCapitalization.None,
+                        autoCorrect = true,
+                        keyboardType = KeyboardType.Email,
+                        imeAction = ImeAction.Next
+                    ),
+                    shape = RoundedCornerShape(20.dp),
+                    value = uiState.email,
+                    isError = emailError,
+                    supportingText = {
+                        if (emailError) Text(text = viewModel.validateEmail(uiState.email))
+                    },
+                    placeholder = { Text(text = "Email") },
+                    onValueChange = { viewModel.onEmailChange(it) },
+                )
 
-            Spacer(modifier = Modifier.height(6.dp))
+                Spacer(modifier = Modifier.height(6.dp))
 
-            OutlinedTextField(
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions(
-                    capitalization = KeyboardCapitalization.None,
-                    autoCorrect = true,
-                    keyboardType = KeyboardType.NumberPassword,
-                    imeAction = ImeAction.Done
-                ),
-                shape = RoundedCornerShape(20.dp),
-                value = uiState.password,
-                isError = passwordError,
-                supportingText = {
-                    if (passwordError)
-                        Text(text = viewModel.validatePassword(uiState.password))
-                },
-                visualTransformation = PasswordVisualTransformation(),
-                placeholder = { Text("Senha") },
-                onValueChange = {
-                    if (it.length <= ConstantsApp.PASSWORD_MAX_NUMBER) viewModel.onPasswordChange(it)
-                }
-            )
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(
+                        capitalization = KeyboardCapitalization.None,
+                        autoCorrect = true,
+                        keyboardType = KeyboardType.NumberPassword,
+                        imeAction = ImeAction.Done
+                    ),
+                    shape = RoundedCornerShape(20.dp),
+                    value = uiState.password,
+                    isError = passwordError,
+                    supportingText = {
+                        if (passwordError)
+                            Text(text = viewModel.validatePassword(uiState.password))
+                    },
+                    visualTransformation = PasswordVisualTransformation(),
+                    placeholder = { Text("Senha") },
+                    onValueChange = {
+                        if (it.length <= ConstantsApp.PASSWORD_MAX_NUMBER) viewModel.onPasswordChange(
+                            it
+                        )
+                    }
+                )
 
-            Spacer(modifier = Modifier.height(10.dp))
+                Spacer(modifier = Modifier.height(10.dp))
 
-            ProgressButton(
-                modifier = Modifier.padding(top = 16.dp).fillMaxWidth(),
-                text = "Cadastrar",
-                isLoading = progressButtonIsActivated,
-                onClick = {
-                    viewModel.validateName(uiState.name)
-                    viewModel.validatePhoneNumber(uiState.phoneNumber)
-                    viewModel.validateEmail(uiState.email)
-                    viewModel.validatePassword(uiState.password)
-                    if (!nameError || !phoneError || !emailError || !passwordError) {
-                        scope.launch {
-                            try {
-                                auth.createUserWithEmailAndPassword(
-                                    email = uiState.email,
-                                    password = uiState.password
-                                )
-                                progressButtonIsActivated = true
-                            } catch (e: Exception) {
-                                progressButtonIsActivated = false
+                ProgressButton(
+                    modifier = Modifier.padding(top = 16.dp).fillMaxWidth(),
+                    text = "Cadastrar",
+                    isLoading = progressButtonIsActivated,
+                    onClick = {
+                        viewModel.validateName(uiState.name)
+                        viewModel.validatePhoneNumber(uiState.phoneNumber)
+                        viewModel.validateEmail(uiState.email)
+                        viewModel.validatePassword(uiState.password)
+                        if (!nameError || !phoneError || !emailError || !passwordError) {
+                            scope.launch {
+                                try {
+                                    progressButtonIsActivated = true
+                                    auth.createUserWithEmailAndPassword(
+                                        email = uiState.email,
+                                        password = uiState.password
+                                    )
+                                    firebaseUser = auth.currentUser
+                                } catch (e: Exception) {
+                                    progressButtonIsActivated = false
+                                    snackBarOnlyMessage(
+                                        snackBarHostState = snackBarHostState,
+                                        coroutineScope = scope,
+                                        message = "Não foi possível criar a sua conta, por favor, tente mais tarde."
+                                    )
+                                }
                             }
                         }
                     }
+                )
+
+                if (firebaseUser != null) {
+                    LaunchedEffect(key1 = true) {
+                        settings.putString(UserPreferences.UID, firebaseUser?.uid.toString())
+                        snackBarOnlyMessage(
+                            snackBarHostState = snackBarHostState,
+                            coroutineScope = scope,
+                            message = "Conta criada com Sucesso! Iremos direcionar ao menu do aplicativo."
+                        )
+                        delay(2000L)
+                        onNavigateToHome()
+                    }
                 }
-            )
+            }
         }
     }
 }
