d08a1855e6a1c8fb422e11609f8b863f5f76c44a
walter <walter.d.retzer@gmail.com>
2024-06-07 13:40:19 -0300

    Ajustado os icon do BottomNavigationBar para o tipo DrawableResource.

--- composeApp/src/commonMain/kotlin/navigation/home/HomeNavigation.kt
+++ composeApp/src/commonMain/kotlin/navigation/home/HomeNavigation.kt
@@ -1,11 +1,9 @@
 package navigation.home
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.List
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.ui.graphics.vector.ImageVector
+import halfmouthappproject.composeapp.generated.resources.Res
+import halfmouthappproject.composeapp.generated.resources.icon_beer
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.ExperimentalResourceApi
 
 enum class NavPath {
     HOME, SEARCH, LIST, PROFILE
@@ -18,19 +16,21 @@ object NavTitle {
     const val PROFILE = "Profile"
 }
 
-open class Item(val path: String, val title: String, val icon: ImageVector)
+@OptIn(ExperimentalResourceApi::class)
+open class Item(val path: String, val title: String, val icon: DrawableResource)
+@OptIn(ExperimentalResourceApi::class)
 sealed class NavItem {
     object Home :
-        Item(path = NavPath.HOME.toString(), title = NavTitle.HOME, icon = Icons.Default.Home)
+        Item(path = NavPath.HOME.toString(), title = NavTitle.HOME, icon = Res.drawable.icon_beer)
 
     object Search :
-        Item(path = NavPath.SEARCH.toString(), title = NavTitle.SEARCH, icon = Icons.Default.Search)
+        Item(path = NavPath.SEARCH.toString(), title = NavTitle.SEARCH, icon = Res.drawable.icon_beer )
 
     object List :
-        Item(path = NavPath.LIST.toString(), title = NavTitle.LIST, icon = Icons.Default.List)
+        Item(path = NavPath.LIST.toString(), title = NavTitle.LIST, icon = Res.drawable.icon_beer)
 
     object Profile :
         Item(
-            path = NavPath.PROFILE.toString(), title = NavTitle.PROFILE, icon = Icons.Default.Person
+            path = NavPath.PROFILE.toString(), title = NavTitle.PROFILE, icon = Res.drawable.icon_beer
         )
 }
