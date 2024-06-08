49bd8e4d80210413daf59f9a8578f7a1eaa686a5
walter <walter.d.retzer@gmail.com>
2024-06-08 12:19:36 -0300

    Ajuste do background da screen do icon de launch do aplicativo no Android

--- composeApp/src/androidMain/kotlin/half/mouth/app/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/half/mouth/app/MainActivity.kt
@@ -1,6 +1,5 @@
 package half.mouth.app
 
-import navigation.NavHostMain
 import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -9,14 +8,19 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
+import navigation.NavHostMain
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val actionBar = actionBar
+        actionBar?.hide()
         WindowCompat.setDecorFitsSystemWindows(window, true)
         window.statusBarColor= Color.BLACK
         WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightStatusBars = false
+
         setContent {
             NavHostMain(
                 darkTheme = isSystemInDarkTheme(),
