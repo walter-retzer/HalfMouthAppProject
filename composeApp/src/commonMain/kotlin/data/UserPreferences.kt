003094114617f6e6e78b35a5264bb9c92d71836c
walter <walter.d.retzer@gmail.com>
2024-06-08 12:33:01 -0300

    Adicionado Keys para salvar nome, email e celular.

--- composeApp/src/commonMain/kotlin/data/UserPreferences.kt
+++ composeApp/src/commonMain/kotlin/data/UserPreferences.kt
@@ -4,5 +4,8 @@ class UserPreferences {
 
     companion object Keys {
         const val UID = "KEY_IS_UID"
+        const val NAME = "KEY_IS_NAME"
+        const val EMAIL = "KEY_IS_EMAIL"
+        const val PHONE = "KEY_IS_PHONE"
     }
 }
