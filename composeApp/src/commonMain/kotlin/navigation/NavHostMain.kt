eadc712820487e7a4922c27a780bb36086084832
walter <walter.d.retzer@gmail.com>
2024-06-09 12:43:42 -0300

    Criado fun para os gráficos de navegação.

--- composeApp/src/commonMain/kotlin/navigation/NavHostMain.kt
+++ composeApp/src/commonMain/kotlin/navigation/NavHostMain.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navigation
 import navigation.NavAnimations.popEnterRightAnimation
 import navigation.NavAnimations.popExitRightAnimation
 import navigation.NavAnimations.slideLeftEnterAnimation
@@ -17,6 +19,7 @@ import presentation.ScreenTheme
 import screens.account.LoginScreen
 import screens.account.SignInScreen
 import screens.home.HomeScreen
+import screens.profile.ProfileScreen
 import screens.splash.SplashScreen
 
 
@@ -34,55 +37,122 @@ fun NavHostMain(
         Surface(modifier = Modifier.fillMaxSize()) {
             NavHost(
                 navController = navController,
-                startDestination = AppNavigation.SplashScreenRoute.name,
+                startDestination = AppGraphNav.LoginGraph.name,
                 enterTransition = slideLeftEnterAnimation,
                 exitTransition = slideLeftExitAnimation,
                 popEnterTransition = popEnterRightAnimation,
                 popExitTransition = popExitRightAnimation
             ) {
-                composable(route = AppNavigation.SplashScreenRoute.name) {
-                    SplashScreen(
-                        onNavigateToLogin = {
-                            navController.navigate(AppNavigation.LoginRoute.name) {
-                                popUpTo(AppNavigation.SplashScreenRoute.name) {
-                                    inclusive = true
-                                }
-                            }
-                        },
-                        onNavigateToHome = {
-                            navController.navigate(AppNavigation.HomeRoute.name){
-                                popUpTo(AppNavigation.SplashScreenRoute.name) {
-                                    inclusive = true
-                                }
-                            }
-                        }
-                    )
-                }
-                composable(route = AppNavigation.LoginRoute.name) {
-                    LoginScreen(
-                        onNavigateToSignIn = {
-                            navController.navigate(AppNavigation.SignInRoute.name)
-                        },
-                        onNavigateToHome = {
-                            navController.navigate(AppNavigation.HomeRoute.name){
-                                popUpTo(AppNavigation.LoginRoute.name) {
-                                    inclusive = true
-                                }
-                            }
-                        }
-                    )
-                }
-                composable(route = AppNavigation.SignInRoute.name) {
-                    SignInScreen(
-                        onNavigateToHome = {
-                            navController.navigate(AppNavigation.HomeRoute.name)
-                        }
-                    )
-                }
-                composable(route = AppNavigation.HomeRoute.name) {
-                    HomeScreen()
-                }
+                loginNavGraph(navController)
+                homeNavGraph(navController)
+                notificationNavGraph(navController)
+                profileNavGraph(navController)
             }
+        }
+    }
+}
+
+
+private fun NavGraphBuilder.loginNavGraph(
+    navController: NavHostController,
+) {
+    navigation(
+        route = AppGraphNav.LoginGraph.name,
+        startDestination = AppNavigation.HomeRoute.name
+    ) {
+        composable(route = AppNavigation.SplashScreenRoute.name) {
+            SplashScreen(
+                onNavigateToLogin = {
+                    navController.navigate(AppNavigation.LoginRoute.name) {
+                        popUpTo(AppNavigation.SplashScreenRoute.name) {
+                            inclusive = true
+                        }
+                    }
+                },
+                onNavigateToHome = {
+                    navController.navigate(AppGraphNav.HomeGraph.name) {
+                        popUpTo(AppNavigation.SplashScreenRoute.name) {
+                            inclusive = true
+                        }
+                    }
+                }
+            )
+        }
+        composable(route = AppNavigation.LoginRoute.name) {
+            LoginScreen(
+                onNavigateToSignIn = {
+                    navController.navigate(AppNavigation.SignInRoute.name)
+                },
+                onNavigateToHome = {
+                    navController.navigate(AppGraphNav.HomeGraph.name){
+                        popUpTo(AppNavigation.LoginRoute.name) {
+                            inclusive = true
+                        }
+                    }
+                }
+            )
+        }
+        composable(route = AppNavigation.SignInRoute.name) {
+            SignInScreen(
+                onNavigateToHome = {
+                    navController.navigate(AppGraphNav.HomeGraph.name)
+                }
+            )
+        }
+        composable(route = AppNavigation.HomeRoute.name) {
+            HomeScreen(
+                onNavigateToProfile = {
+                    navController.navigate(AppNavigation.ProfileRoute.name)
+                },
+                navController = navController
+            )
+        }
+    }
+}
+
+
+private fun NavGraphBuilder.homeNavGraph(
+    navController: NavHostController,
+) {
+    navigation(
+        route = AppGraphNav.HomeGraph.name,
+        startDestination = AppNavigation.ProfileRoute.name
+    ) {
+
+        composable(route = AppNavigation.ProfileRoute.name) {
+            HomeScreen(
+                onNavigateToProfile = {
+                    navController.navigate(AppNavigation.ProfileRoute.name)
+                },
+                navController = navController
+            )
+        }
+    }
+}
+private fun NavGraphBuilder.notificationNavGraph(
+    navController: NavHostController,
+) {
+    navigation(
+        route = AppGraphNav.ProfileGraph.name,
+        startDestination = AppNavigation.ProfileRoute.name
+    ) {
+
+        composable(route = AppNavigation.ProfileRoute.name) {
+            ProfileScreen()
+        }
+    }
+}
+
+private fun NavGraphBuilder.profileNavGraph(
+    navController: NavHostController,
+) {
+    navigation(
+        route = AppGraphNav.NotificationGraph.name,
+        startDestination = AppNavigation.ProfileRoute.name
+    ) {
+
+        composable(route = AppNavigation.ProfileRoute.name) {
+            ProfileScreen()
         }
     }
 }
