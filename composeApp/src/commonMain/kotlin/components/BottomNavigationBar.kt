8add8c5b6ca3666a399f81c93beb97cddab011c7
walter <walter.d.retzer@gmail.com>
2024-06-09 12:44:49 -0300

    Testando rota de navegação pelos icons da BottomNavigationBar

--- composeApp/src/commonMain/kotlin/components/BottomNavigationBar.kt
+++ composeApp/src/commonMain/kotlin/components/BottomNavigationBar.kt
@@ -1,5 +1,6 @@
 package components
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -10,14 +11,25 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavGraph
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import navigation.AppGraphNav
+import navigation.AppNavigation
+import navigation.NavHostMain
 import navigation.home.NavItem
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.vectorResource
+import screens.profile.ProfileScreen
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
-fun BottomNavigationBar(navController: NavHostController) {
+fun BottomNavigationBar(
+    navController: NavHostController,
+    onNavigateToProfile: () -> Unit
+) {
+
     val navItems = listOf(NavItem.Home, NavItem.Production, NavItem.Notification, NavItem.Profile)
     var selectedItem by rememberSaveable { mutableStateOf(0) }
 
@@ -32,14 +44,14 @@ fun BottomNavigationBar(navController: NavHostController) {
                 label = { Text(item.title) },
                 selected = selectedItem == index,
                 onClick = {
-                    selectedItem = index
-                    navController.navigate(item.path) {
-                        navController.graph.startDestinationRoute?.let { route ->
-                            popUpTo(route) { saveState = true }
-                        }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
+                            navController.navigate(AppGraphNav.ProfileGraph.name)
+//                    navController.navigate(item.path) {
+//                        navController.graph.startDestinationRoute?.let { route ->
+//                            popUpTo(route) { saveState = true }
+//                        }
+//                        launchSingleTop = true
+//                        restoreState = true
+//                    }
                 }
             )
         }
