51189cd3f184205ccc00a8ca401b22ddfa81a800
walter <walter.d.retzer@gmail.com>
2024-06-09 12:42:54 -0300

    Incluido class AppGraphNav

--- composeApp/src/commonMain/kotlin/navigation/AppNavigation.kt
+++ composeApp/src/commonMain/kotlin/navigation/AppNavigation.kt
@@ -1,7 +1,13 @@
 package navigation
 
 import halfmouthappproject.composeapp.generated.resources.Res
+import halfmouthappproject.composeapp.generated.resources.home_graph_name
 import halfmouthappproject.composeapp.generated.resources.home_name
+import halfmouthappproject.composeapp.generated.resources.login_graph_name
+import halfmouthappproject.composeapp.generated.resources.notification_graph_name
+import halfmouthappproject.composeapp.generated.resources.production_graph_name
+import halfmouthappproject.composeapp.generated.resources.profile_graph_name
+import halfmouthappproject.composeapp.generated.resources.profile_name
 import halfmouthappproject.composeapp.generated.resources.screen_login_name
 import halfmouthappproject.composeapp.generated.resources.sign_in_name
 import halfmouthappproject.composeapp.generated.resources.splash_screen_name
@@ -14,4 +20,14 @@ enum class AppNavigation(val title: StringResource) {
     LoginRoute(title = Res.string.screen_login_name),
     SignInRoute(title = Res.string.sign_in_name),
     HomeRoute(title = Res.string.home_name),
+    ProfileRoute(title = Res.string.profile_name),
+}
+
+@OptIn(ExperimentalResourceApi::class)
+enum class AppGraphNav(val title: StringResource) {
+    LoginGraph(title = Res.string.login_graph_name),
+    HomeGraph(title = Res.string.home_graph_name),
+    ProfileGraph(title = Res.string.profile_graph_name),
+    ProductionGraph(title = Res.string.production_graph_name),
+    NotificationGraph(title = Res.string.notification_graph_name),
 }
