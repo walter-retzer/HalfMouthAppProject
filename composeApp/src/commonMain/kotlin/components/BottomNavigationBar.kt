ade78ce001738d2fdf4f167b254d8dd0031848b5
walter <walter.d.retzer@gmail.com>
2024-06-10 10:01:30 -0300

    Ajuste quando o  botão back do Android for pressionado.

--- composeApp/src/commonMain/kotlin/components/BottomNavigationBar.kt
+++ composeApp/src/commonMain/kotlin/components/BottomNavigationBar.kt
@@ -1,6 +1,5 @@
 package components
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -11,17 +10,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
-import androidx.navigation.NavGraph
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import navigation.AppGraphNav
-import navigation.AppNavigation
-import navigation.NavHostMain
+import androidx.navigation.compose.currentBackStackEntryAsState
 import navigation.home.NavItem
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.vectorResource
-import screens.profile.ProfileScreen
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
@@ -32,25 +25,30 @@ fun BottomNavigationBar(
 
     val navItems = listOf(NavItem.Home, NavItem.Production, NavItem.Notification, NavItem.Profile)
     var selectedItem by rememberSaveable { mutableStateOf(0) }
+    val currentBackStack by navController.currentBackStackEntryAsState()
+    val currentDestination = currentBackStack?.destination?.route
 
     NavigationBar(
         containerColor = Color.Black,
         contentColor = Color.Black
     ) {
         navItems.forEachIndexed { index, item ->
+            println(navController.graph.startDestinationRoute)
+            println(item.pathRoute)
             NavigationBarItem(
                 alwaysShowLabel = true,
                 icon = { Icon(vectorResource( item.icon), contentDescription = item.title) },
                 label = { Text(item.title) },
-                selected = selectedItem == index,
+                selected = navController.graph.startDestinationRoute == item.pathRoute,
                 onClick = {
-                            navController.navigate(AppGraphNav.ProfileGraph.name)
+                            navController.navigate(item.pathRoute)
+
 //                    navController.navigate(item.path) {
 //                        navController.graph.startDestinationRoute?.let { route ->
 //                            popUpTo(route) { saveState = true }
 //                        }
-//                        launchSingleTop = true
-//                        restoreState = true
+//                    launchSingleTop = true
+//                    restoreState = true
 //                    }
                 }
             )
