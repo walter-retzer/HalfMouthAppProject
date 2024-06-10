324e117861bd6a86c970ba7a94852dfb48b2be5f
walter <walter.d.retzer@gmail.com>
2024-06-10 15:34:26 -0300

    Adicionado fun formattedAsPhone para formatar string com a máscara do celular.

--- composeApp/src/commonMain/kotlin/util/MaskVisualTransformation.kt
+++ composeApp/src/commonMain/kotlin/util/MaskVisualTransformation.kt
@@ -45,3 +45,22 @@ class MaskVisualTransformation(private val mask: String) : VisualTransformation 
         const val PHONE = "(##) #####-####"
     }
 }
+
+fun String.formattedAsPhone(): String {
+    return when (length) {
+        11 -> "(" + substring(0, 2) + ") " +
+                substring(2, 3) + " " +
+                substring(3, 7) + "-" +
+                substring(7, length)
+        10 -> "(" + substring(0, 2) + ") " +
+                substring(2, 6) + "-" +
+                substring(6, length)
+
+        14 -> substring(0, 3) + " (" + substring(3, 5) + ") " +
+                substring(5, 7) +
+                substring(7, 10) + "-" +
+                substring(10, length)
+
+        else -> this
+    }
+}
