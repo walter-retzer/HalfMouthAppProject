c921cb5447c3ecc4cd6fda1b2b84e227b8a98ced
walter <walter.d.retzer@gmail.com>
2024-06-10 09:54:57 -0300

    Ajustado nomes das rotas nas classes AppNavigation e AppGraphNav

--- composeApp/src/commonMain/kotlin/navigation/AppNavigation.kt
+++ composeApp/src/commonMain/kotlin/navigation/AppNavigation.kt
@@ -2,32 +2,30 @@ package navigation
 
 import halfmouthappproject.composeapp.generated.resources.Res
 import halfmouthappproject.composeapp.generated.resources.home_graph_name
-import halfmouthappproject.composeapp.generated.resources.home_name
+import halfmouthappproject.composeapp.generated.resources.home_route
 import halfmouthappproject.composeapp.generated.resources.login_graph_name
-import halfmouthappproject.composeapp.generated.resources.notification_graph_name
-import halfmouthappproject.composeapp.generated.resources.production_graph_name
-import halfmouthappproject.composeapp.generated.resources.profile_graph_name
-import halfmouthappproject.composeapp.generated.resources.profile_name
-import halfmouthappproject.composeapp.generated.resources.screen_login_name
-import halfmouthappproject.composeapp.generated.resources.sign_in_name
-import halfmouthappproject.composeapp.generated.resources.splash_screen_name
+import halfmouthappproject.composeapp.generated.resources.login_route
+import halfmouthappproject.composeapp.generated.resources.notification_route
+import halfmouthappproject.composeapp.generated.resources.production_route
+import halfmouthappproject.composeapp.generated.resources.profile_route
+import halfmouthappproject.composeapp.generated.resources.sign_in_route
+import halfmouthappproject.composeapp.generated.resources.splash_screen_route
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.StringResource
 
 @OptIn(ExperimentalResourceApi::class)
 enum class AppNavigation(val title: StringResource) {
-    SplashScreenRoute(title = Res.string.splash_screen_name),
-    LoginRoute(title = Res.string.screen_login_name),
-    SignInRoute(title = Res.string.sign_in_name),
-    HomeRoute(title = Res.string.home_name),
-    ProfileRoute(title = Res.string.profile_name),
+    SplashScreenRoute(title = Res.string.splash_screen_route),
+    LoginRoute(title = Res.string.login_route),
+    SignInRoute(title = Res.string.sign_in_route),
+    HomeRoute(title = Res.string.home_route),
+    ProfileRoute(title = Res.string.profile_route),
+    ProductionRoute(title = Res.string.production_route),
+    NotificationRoute(title = Res.string.notification_route),
 }
 
 @OptIn(ExperimentalResourceApi::class)
 enum class AppGraphNav(val title: StringResource) {
     LoginGraph(title = Res.string.login_graph_name),
     HomeGraph(title = Res.string.home_graph_name),
-    ProfileGraph(title = Res.string.profile_graph_name),
-    ProductionGraph(title = Res.string.production_graph_name),
-    NotificationGraph(title = Res.string.notification_graph_name),
 }
