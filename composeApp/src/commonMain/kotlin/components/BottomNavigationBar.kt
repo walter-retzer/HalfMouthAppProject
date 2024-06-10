47a5a942368ca0b299a8c4b8da783a7249e30376
walter <walter.d.retzer@gmail.com>
2024-06-10 10:12:03 -0300

    Implementado os ajustes do BottomAppBar na fun AppBottomNavigationBar

--- composeApp/src/commonMain/kotlin/components/BottomNavigationBar.kt
+++ composeApp/src/commonMain/kotlin/components/BottomNavigationBar.kt
@@ -1,57 +1,52 @@
 package components
-
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
-import navigation.home.NavItem
+import androidx.navigation.navOptions
+import navigation.home.Item
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.vectorResource
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
-fun BottomNavigationBar(
-    navController: NavHostController,
-    onNavigateToProfile: () -> Unit
+fun AppBottomNavigationBar(
+    navItems: List<Item>,
+    navController: NavHostController
 ) {
-
-    val navItems = listOf(NavItem.Home, NavItem.Production, NavItem.Notification, NavItem.Profile)
-    var selectedItem by rememberSaveable { mutableStateOf(0) }
     val currentBackStack by navController.currentBackStackEntryAsState()
     val currentDestination = currentBackStack?.destination?.route
 
-    NavigationBar(
+    BottomAppBar(
+        actions = {
+            navItems.forEach { item ->
+                NavigationBarItem(
+                    selected = currentDestination == item.pathRoute,
+                    onClick = {
+                        navController.navigate(item.pathRoute,
+                            navOptions {
+                                launchSingleTop = true
+                                navController.graph.startDestinationRoute?.let { route ->
+                                    popUpTo(route) { saveState = true }
+                                }
+                            }
+                        )
+                    },
+                    icon = {
+                        Icon(vectorResource( item.icon), contentDescription = item.title)
+                    },
+                    label = {
+                        Text(text = item.title)
+                    }
+                )
+            }
+        },
         containerColor = Color.Black,
         contentColor = Color.Black
-    ) {
-        navItems.forEachIndexed { index, item ->
-            println(navController.graph.startDestinationRoute)
-            println(item.pathRoute)
-            NavigationBarItem(
-                alwaysShowLabel = true,
-                icon = { Icon(vectorResource( item.icon), contentDescription = item.title) },
-                label = { Text(item.title) },
-                selected = navController.graph.startDestinationRoute == item.pathRoute,
-                onClick = {
-                            navController.navigate(item.pathRoute)
-
-//                    navController.navigate(item.path) {
-//                        navController.graph.startDestinationRoute?.let { route ->
-//                            popUpTo(route) { saveState = true }
-//                        }
-//                    launchSingleTop = true
-//                    restoreState = true
-//                    }
-                }
-            )
-        }
-    }
+    )
 }
