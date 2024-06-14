a13321e1b3c196a3080b5793f46d1646584cfaf6
walter <walter.d.retzer@gmail.com>
2024-06-14 18:15:16 -0300

    Contante da string de informação de erro do request/response da api.

--- composeApp/src/commonMain/kotlin/util/ConstantsApp.kt
+++ composeApp/src/commonMain/kotlin/util/ConstantsApp.kt
@@ -5,5 +5,6 @@ class ConstantsApp {
             val PASSWORD_MAX_NUMBER = 6
             val PHONE_MAX_NUMBER = 11
             val NAME_MAX_NUMBER = 25
+            val ERROR_API = "Não foi possível exibir os dados, por favor, tente mais tarde."
         }
 }
