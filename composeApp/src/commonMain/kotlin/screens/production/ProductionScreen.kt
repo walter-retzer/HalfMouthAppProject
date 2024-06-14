9cb318e58ff7eb30269cd9918f4b014d6bf3e4e7
walter <walter.d.retzer@gmail.com>
2024-06-14 14:13:03 -0300

    Implementação dos status da sealed interface da ProductionViewModel

--- composeApp/src/commonMain/kotlin/screens/production/ProductionScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/production/ProductionScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -22,6 +23,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -36,14 +38,18 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import components.AppToolbarLarge
+import components.MyAppCircularProgressIndicator
+import data.Feeds
 import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
 import halfmouthappproject.composeapp.generated.resources.Res
 import halfmouthappproject.composeapp.generated.resources.icon_beer
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
+import theme.mainYellowColor
 import theme.onSurfaceVariantLight
 import viewmodel.ProductionViewModel
+import viewmodel.ProductionViewState
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalResourceApi::class)
 @Composable
@@ -55,13 +61,9 @@ fun ProductionScreen() {
             ProductionViewModel()
         }
     )
-    val uiState by viewModel.uiState.collectAsState()
-
-    val scrollBehavior =
-        TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
-    val scope = rememberCoroutineScope()
+    val uiState1 by viewModel.uiState1.collectAsState()
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
     val snackBarHostState = remember { SnackbarHostState() }
-    val listFeedReceive = viewModel.newFeedList
 
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
@@ -76,123 +78,134 @@ fun ProductionScreen() {
             )
         }
     ) { innerPadding ->
+        LazyColumn(modifier = Modifier.fillMaxWidth(), contentPadding = innerPadding) {
 
-        LazyColumn(modifier = Modifier.fillMaxWidth()) {
-            items(listFeedReceive) {
-                Spacer(modifier = Modifier.height(4.dp))
-                Card(
-                    modifier = Modifier
-                        .padding(
-                            top = 7.dp,
-                            bottom = 7.dp,
-                            start = 12.dp,
-                            end = 12.dp
-                        ),
-                    shape = RoundedCornerShape(16.dp),
-                ) {
-
-                    Row(
-                        modifier = Modifier
-                            .padding(
-                                top = 24.dp,
-                                end = 16.dp,
-                            )
-                            .fillMaxSize(),
-                        horizontalArrangement = Arrangement.End,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Image(
-                            painter = painterResource(Res.drawable.icon_beer),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .padding(5.dp)
-                                .clickable(onClick = { }),
-                            alignment = Alignment.BottomEnd,
-                        )
-                    }
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                    ) {
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
+            when (val state = uiState1){
+                is ProductionViewState.Dashboard -> {
+                    items(state.sensorsValues) {
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Card(
                             modifier = Modifier
                                 .padding(
-                                    start = 16.dp,
-                                    top = 16.dp,
-                                    end = 0.dp,
-                                    bottom = 4.dp
-                                )
-                                .fillMaxSize(),
+                                    top = 7.dp,
+                                    bottom = 7.dp,
+                                    start = 12.dp,
+                                    end = 12.dp
+                                ),
+                            shape = RoundedCornerShape(16.dp),
                         ) {
+
+                            Row(
+                                modifier = Modifier
+                                    .padding(
+                                        top = 24.dp,
+                                        end = 16.dp,
+                                    )
+                                    .fillMaxSize(),
+                                horizontalArrangement = Arrangement.End,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Image(
+                                    painter = painterResource(Res.drawable.icon_beer),
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .padding(5.dp)
+                                        .clickable(onClick = { }),
+                                    alignment = Alignment.BottomEnd,
+                                )
+                            }
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                            ) {
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    modifier = Modifier
+                                        .padding(
+                                            start = 16.dp,
+                                            top = 16.dp,
+                                            end = 0.dp,
+                                            bottom = 4.dp
+                                        )
+                                        .fillMaxSize(),
+                                ) {
 //                            val drawable =
 //                                if (it.fieldName.toString() == "CAMARA FRIA") Res.drawable.icon_freezer
 //                                else if (it.fieldName.toString() == "CHILLER") Res.drawable.icon_freezer
 //                                else if (it.fieldName.toString() == "BOMBA RECIRCULAÇÃO") Res.drawable.icon_freezer
 //                                else Res.drawable.icon_thermostat
-                            Image(
-                                painter = painterResource(Res.drawable.icon_beer),
-                                contentDescription = null,
-                                modifier = Modifier.padding(5.dp)
-                            )
-                            val name =
-                                if (it.fieldName.toString() == "BOMBA RECIRCULAÇÃO") "MBO-001"
-                                else it.fieldName.toString()
-                            Text(
-                                text = name,
-                                style = TextStyle(
-                                    color = Color.Black,
-                                    fontSize = 20.sp,
-                                    fontWeight = FontWeight.Bold,
-                                    fontFamily = FontFamily.SansSerif
-                                )
-                            )
-                            val valueFormatted = it.fieldValue.toString()
-                            val text =
-                                when (it.fieldValue) {
-                                    "0.00000" -> " = 0"
-                                    "1.00000" -> " = 1"
-                                    else -> " = ${
-                                        valueFormatted.replace(
-                                            ",",
-                                            "."
-                                        )
-                                    }°C"
-                                }
-                            Text(
-                                text = text,
-                                style = TextStyle(
-                                    color = Color.Black,
-                                    fontSize = 20.sp,
-                                    fontWeight = FontWeight.Bold,
-                                )
-                            )
-                        }
-
-                        Row(
-                            modifier = Modifier
-                                .padding(
-                                    start = 24.dp,
-                                    top = 0.dp,
-                                    end = 16.dp,
-                                    bottom = 8.dp
-                                )
-                                .fillMaxSize(),
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                        ) {
-                            if (it.fieldData != null) {
-                                val date = it.fieldData.toString()
-                                Text(
-                                    text = "Data: $date",
-                                    style = TextStyle(
-                                        color = onSurfaceVariantLight,
-                                        fontSize = 14.sp,
-                                        fontWeight = FontWeight.Normal,
+                                    Image(
+                                        painter = painterResource(Res.drawable.icon_beer),
+                                        contentDescription = null,
+                                        modifier = Modifier.padding(5.dp)
                                     )
-                                )
+                                    val name =
+                                        if (it.fieldName.toString() == "BOMBA RECIRCULAÇÃO") "MBO-001"
+                                        else it.fieldName.toString()
+                                    Text(
+                                        text = name,
+                                        style = TextStyle(
+                                            color = Color.Black,
+                                            fontSize = 20.sp,
+                                            fontWeight = FontWeight.Bold,
+                                            fontFamily = FontFamily.SansSerif
+                                        )
+                                    )
+                                    val valueFormatted = it.fieldValue.toString()
+                                    val text =
+                                        when (it.fieldValue) {
+                                            "0.00000" -> " = 0"
+                                            "1.00000" -> " = 1"
+                                            else -> " = ${
+                                                valueFormatted.replace(
+                                                    ",",
+                                                    "."
+                                                )
+                                            }°C"
+                                        }
+                                    Text(
+                                        text = text,
+                                        style = TextStyle(
+                                            color = Color.Black,
+                                            fontSize = 20.sp,
+                                            fontWeight = FontWeight.Bold,
+                                        )
+                                    )
+                                }
+
+                                Row(
+                                    modifier = Modifier
+                                        .padding(
+                                            start = 24.dp,
+                                            top = 0.dp,
+                                            end = 16.dp,
+                                            bottom = 8.dp
+                                        )
+                                        .fillMaxSize(),
+                                    horizontalArrangement = Arrangement.SpaceBetween,
+                                ) {
+                                    if (it.fieldData != null) {
+                                        val date = it.fieldData.toString()
+                                        Text(
+                                            text = "Data: $date",
+                                            style = TextStyle(
+                                                color = onSurfaceVariantLight,
+                                                fontSize = 14.sp,
+                                                fontWeight = FontWeight.Normal,
+                                            )
+                                        )
+                                    }
+                                }
                             }
                         }
                     }
+                }
+                is ProductionViewState.Error -> {}
+                is ProductionViewState.Loading -> {
+//                    CircularProgressIndicator(
+//                        modifier = Modifier,
+//                        color = mainYellowColor
+//                    )
                 }
             }
         }
