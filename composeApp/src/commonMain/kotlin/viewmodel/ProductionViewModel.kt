1babde6847b8aa66057bbed85573968368e0da59
walter <walter.d.retzer@gmail.com>
2024-06-14 10:50:00 -0300

    Ajuste para iniciar a fun e fun para coletar os valores da Class Feed.

--- composeApp/src/commonMain/kotlin/viewmodel/ProductionViewModel.kt
+++ composeApp/src/commonMain/kotlin/viewmodel/ProductionViewModel.kt
@@ -1,23 +1,20 @@
 package viewmodel
 
+import data.Feeds
 import data.ThingSpeakResponse
 import dev.icerock.moko.mvvm.viewmodel.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import network.ApiService
-import network.ResultType
+import network.ResultNetwork
 
 
 class ProductionViewModel : ViewModel() {
 
     private val service = ApiService.create()
     private val results = "2"
-
-    var res: ThingSpeakResponse = ThingSpeakResponse(
-        channel = null,
-        feeds = emptyList()
-    )
+    var newFeedList = mutableListOf(Feeds())
 
     private val _uiState = MutableStateFlow(
         ThingSpeakResponse(
@@ -27,19 +24,73 @@ class ProductionViewModel : ViewModel() {
     )
     val uiState = _uiState.asStateFlow()
 
+    init {
+        loadFirstShimmer()
+    }
+
     fun loadFirstShimmer() {
         viewModelScope.launch {
-            val resp =   service.getThingSpeakValues(results = results)
-            println(resp)
-            res = handleResult(resp)
-            _uiState.value = res
+            val response = service.getThingSpeakValues(results = results)
+            _uiState.value = handleResult(response)
+            adjustValuesInListFeed(mutableListOf(uiState.value))
         }
     }
 
-    private fun handleResult(result: ResultType<ThingSpeakResponse>): ThingSpeakResponse {
+    private fun handleResult(result: ResultNetwork<ThingSpeakResponse>): ThingSpeakResponse {
         return when (result) {
-            is ResultType.Failure -> throw result.exception
-            is ResultType.Success -> result.data
+            is ResultNetwork.Failure -> throw result.exception
+            is ResultNetwork.Success -> result.data
         }
     }
+
+    private fun adjustValuesInListFeed(listReceive: List<ThingSpeakResponse>): MutableList<Feeds> {
+        listReceive.forEach { response ->
+            val i1 = if (response.feeds.first()?.field1 == null) 1 else 0
+            val i2 = if (response.feeds.first()?.field5 == null) 1 else 0
+            newFeedList = mutableListOf(
+                Feeds(
+                    fieldName = response.channel?.field1,
+                    fieldValue = response.feeds[i1]?.field1,
+                    fieldData = response.feeds[i1]?.created_at
+                ),
+                Feeds(
+                    fieldName = response.channel?.field2,
+                    fieldValue = response.feeds[i1]?.field2,
+                    fieldData = response.feeds[i1]?.created_at
+                ),
+                Feeds(
+                    fieldName = response.channel?.field3,
+                    fieldValue = response.feeds[i1]?.field3,
+                    fieldData = response.feeds[i1]?.created_at
+                ),
+                Feeds(
+                    fieldName = response.channel?.field4,
+                    fieldValue = response.feeds[i1]?.field4,
+                    fieldData = response.feeds[i1]?.created_at
+                ),
+                Feeds(
+                    fieldName = response.channel?.field5,
+                    fieldValue = response.feeds[i2]?.field5,
+                    fieldData = response.feeds[i2]?.created_at
+                ),
+                Feeds(
+                    fieldName = response.channel?.field6,
+                    fieldValue = response.feeds[i1]?.field6,
+                    fieldData = response.feeds[i1]?.created_at
+                ),
+                Feeds(
+                    fieldName = response.channel?.field7,
+                    fieldValue = response.feeds[i1]?.field7,
+                    fieldData = response.feeds[i1]?.created_at
+                ),
+                Feeds(
+                    fieldName = response.channel?.field8,
+                    fieldValue = response.feeds[i2]?.field8,
+                    fieldData = response.feeds[i2]?.created_at
+                ),
+            )
+        }
+        return newFeedList
+    }
 }
+
