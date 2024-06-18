27afb895d9222b2b21c94f89a2de8ed8159af195
walter <walter.d.retzer@gmail.com>
2024-06-18 20:59:09 -0300

    Adicionado interface LoginUserViewState  com os estados para a tela de Login

--- composeApp/src/commonMain/kotlin/viewmodel/LoginUserViewModel.kt
+++ composeApp/src/commonMain/kotlin/viewmodel/LoginUserViewModel.kt
@@ -1,12 +1,24 @@
 package viewmodel
 
+import com.russhwolf.settings.Settings
+import data.UserPreferences
+import dev.gitlive.firebase.Firebase
+import dev.gitlive.firebase.auth.FirebaseUser
+import dev.gitlive.firebase.auth.auth
 import dev.icerock.moko.mvvm.viewmodel.ViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import util.ConstantsApp
 
-class LoginUserViewModel : ViewModel(){
+class LoginUserViewModel : ViewModel() {
+
+    private val settingsPref = Settings()
+    private val authService = Firebase.auth
+    private var firebaseUser: FirebaseUser? = null
 
     private val _emailError = MutableStateFlow(false)
     val emailError = _emailError.asStateFlow()
@@ -14,17 +26,42 @@ class LoginUserViewModel : ViewModel(){
     private val _passwordError = MutableStateFlow(false)
     val passwordError = _passwordError.asStateFlow()
 
-    private val _uiState = MutableStateFlow(LoginUserData())
-    val uiState = _uiState.asStateFlow()
+    private val _userLoginState = MutableStateFlow(LoginUserData())
+    val userLoginState = _userLoginState.asStateFlow()
+
+    private val _uiState = MutableStateFlow<LoginUserViewState>(LoginUserViewState.Dashboard)
+    val uiState: StateFlow<LoginUserViewState> = _uiState.asStateFlow()
+
+    fun onSignIn(email: String, password: String) {
+        _uiState.value = LoginUserViewState.Loading
+        viewModelScope.launch {
+            try {
+                authService.signInWithEmailAndPassword(
+                    email = email,
+                    password = password
+                )
+                delay(3000L)
+                firebaseUser = authService.currentUser
+                settingsPref.putString(UserPreferences.UID, firebaseUser?.uid.toString())
+                if (firebaseUser != null) _uiState.value =
+                    LoginUserViewState.Success(ConstantsApp.SUCCESS_SIGN_IN)
+                else _uiState.value = LoginUserViewState.Error(ConstantsApp.ERROR_SIGN_IN)
+            } catch (e: Exception) {
+                println(e)
+                _uiState.value = LoginUserViewState.Error(ConstantsApp.ERROR_SIGN_IN)
+            }
+        }
+    }
+
 
     fun onEmailChange(newValue: String) {
-        _uiState.update { it.copy(email = newValue) }
+        _userLoginState.update { it.copy(email = newValue) }
         //reset error when the user types another character
         if (newValue.isNotBlank()) _emailError.value = false
     }
 
     fun onPasswordChange(newValue: String) {
-        _uiState.update { it.copy(password = newValue) }
+        _userLoginState.update { it.copy(password = newValue) }
         //reset error when the user types another character
         if (newValue.isNotBlank()) _passwordError.value = false
     }
@@ -50,3 +87,10 @@ data class LoginUserData(
     val password: String = "",
 )
 
+sealed interface LoginUserViewState {
+    data object Loading : LoginUserViewState
+    data object Dashboard : LoginUserViewState
+    data class Success(val message: String) : LoginUserViewState
+    data class Error(val message: String) : LoginUserViewState
+
+}
