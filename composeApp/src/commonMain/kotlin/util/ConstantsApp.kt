f72c4631f47ac5f0100c09fda2b4d0488bff22bd
walter <walter.d.retzer@gmail.com>
2024-06-18 20:55:30 -0300

    Adicionado constantes de strings

--- composeApp/src/commonMain/kotlin/util/ConstantsApp.kt
+++ composeApp/src/commonMain/kotlin/util/ConstantsApp.kt
@@ -6,6 +6,8 @@ class ConstantsApp {
             val PHONE_MAX_NUMBER = 11
             val NAME_MAX_NUMBER = 25
             val ERROR_API = "Não foi possível exibir os dados, por favor, tente mais tarde."
+            val ERROR_SIGN_IN = "Não foi possível acessar a sua conta, por favor, tente mais tarde."
+            val SUCCESS_SIGN_IN = "Login realizado com Sucesso!"
             val ERROR_DELETE_ACCOUNT = "Não foi possível deletar a sua conta, por favor, tente mais tarde."
             val ERROR_SIGN_OUT = "Não foi possível sair da sua conta, por favor, tente mais tarde"
             val SUCCESS_DELETE_ACCOUNT = "Sua conta foi excluída com sucesso."
