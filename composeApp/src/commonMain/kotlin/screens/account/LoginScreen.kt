56033fc2a708ba0e20b51e993d9a9e808cd26037
walter <walter.d.retzer@gmail.com>
2024-06-19 16:33:57 -0300

    Fun para exibir o BottomSheet para o envio do resete da senha.

--- composeApp/src/commonMain/kotlin/screens/account/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/account/LoginScreen.kt
@@ -8,24 +8,35 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.displayCutout
+import androidx.compose.foundation.layout.displayCutoutPadding
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -33,6 +44,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -82,6 +94,7 @@ fun LoginScreen(
     val snackBarHostState = remember { SnackbarHostState() }
     var progressButtonIsActivated by remember { mutableStateOf(false) }
     var snackBarIsActivated by remember { mutableStateOf(false) }
+    var showBottomSheet by remember { mutableStateOf(false) }
 
 
     Scaffold(
@@ -176,6 +189,13 @@ fun LoginScreen(
                     }
                 )
 
+                Text(
+                    modifier = Modifier.align(alignment = Alignment.End),
+                    text = "Esqueceu a senha?",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.secondary,
+                )
+
                 Spacer(modifier = Modifier.height(10.dp))
 
                 ProgressButton(
@@ -194,9 +214,9 @@ fun LoginScreen(
                 Spacer(modifier = Modifier.height(16.dp))
 
                 Text(
-                    modifier = Modifier,
+                    modifier = Modifier.clickable { showBottomSheet = true },
                     text = "Ainda não tem uma conta?",
-                    style = MaterialTheme.typography.bodyLarge,
+                    style = MaterialTheme.typography.bodyMedium,
                     color = MaterialTheme.colorScheme.secondary,
                 )
 
@@ -209,9 +229,16 @@ fun LoginScreen(
                     color = mainYellowColor,
                 )
 
+                if (showBottomSheet) {
+                    BottomSheet(viewModel) {
+                        showBottomSheet = false
+                    }
+                }
 
                 when (val state = uiState) {
                     is LoginUserViewState.Dashboard -> { }
+
+                    is LoginUserViewState.Loading -> { progressButtonIsActivated = true }
 
                     is LoginUserViewState.Error -> {
                         progressButtonIsActivated = false
@@ -228,8 +255,6 @@ fun LoginScreen(
                         }
                     }
 
-                    is LoginUserViewState.Loading -> { progressButtonIsActivated = true }
-
                     is LoginUserViewState.Success -> {
                         progressButtonIsActivated = false
                         LaunchedEffect(key1 = true) {
@@ -242,8 +267,91 @@ fun LoginScreen(
                             onNavigateToHome()
                         }
                     }
+
+                    is LoginUserViewState.SuccessResetPassword -> {
+                        println("Success RESET")
+                        snackBarIsActivated = true
+
+                        LaunchedEffect(snackBarIsActivated) {
+                            snackBarOnlyMessage(
+                                snackBarHostState = snackBarHostState,
+                                coroutineScope = scope,
+                                message = state.message,
+                                duration = SnackbarDuration.Long
+                            )
+                            snackBarIsActivated = false
+                        }
+                    }
                 }
             }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomSheet(viewModel: LoginUserViewModel, onDismiss: () -> Unit) {
+
+    val modalBottomSheetState = rememberModalBottomSheetState()
+    var email by rememberSaveable { mutableStateOf("") }
+
+    val userLogin by viewModel.userLoginState.collectAsState()
+    val uiState by viewModel.uiState.collectAsState()
+    val emailError by viewModel.emailError.collectAsState()
+
+    ModalBottomSheet(
+        //modifier = Modifier.displayCutoutPadding(),
+        onDismissRequest = { onDismiss() },
+        sheetState = modalBottomSheetState,
+        dragHandle = { BottomSheetDefaults.DragHandle() },
+        //windowInsets = WindowInsets.displayCutout
+    ) {
+
+        Column(
+            modifier = Modifier.padding(16.dp).height(250.dp)
+        ) {
+
+            Text(
+                modifier = Modifier.align(alignment = Alignment.CenterHorizontally),
+                text = "Recuperar senha",
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.secondary
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                modifier = Modifier.align(alignment = Alignment.Start),
+                text = "Informe seu email:",
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.secondary,
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(
+                    capitalization = KeyboardCapitalization.None,
+                    autoCorrect = true,
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Done
+                ),
+                shape = RoundedCornerShape(20.dp),
+                value = email,
+                placeholder = { Text(text = "Email") },
+                onValueChange = { viewModel.onEmailForResetPasswordChange(it) }
+            )
+
+            ProgressButton(
+                modifier = Modifier.padding(top = 16.dp, bottom = 50.dp).fillMaxWidth(),
+                text = "Enviar",
+                isLoading = false,
+                onClick = {
+                    viewModel.onResetPassword(email)
+                    onDismiss()
+                }
+            )
         }
     }
 }
