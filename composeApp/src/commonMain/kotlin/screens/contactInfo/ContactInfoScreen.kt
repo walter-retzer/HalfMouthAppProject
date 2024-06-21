dd02c891e96a356239422de2be3339045d0943c4
walter <walter.d.retzer@gmail.com>
2024-06-21 16:07:02 -0300

    Implementado o Método OpenGoogleMaps para abrir o Google Maps quando a imagem da localização for clicada.

--- composeApp/src/commonMain/kotlin/screens/contactInfo/ContactInfoScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/contactInfo/ContactInfoScreen.kt
@@ -1,5 +1,7 @@
 package screens.contactInfo
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
@@ -10,6 +12,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -27,11 +30,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import components.AppToolbarLarge
 import halfmouthappproject.composeapp.generated.resources.Res
+import halfmouthappproject.composeapp.generated.resources.address
 import halfmouthappproject.composeapp.generated.resources.icon_email
 import halfmouthappproject.composeapp.generated.resources.icon_location
 import halfmouthappproject.composeapp.generated.resources.icon_phone
@@ -39,8 +45,10 @@ import halfmouthappproject.composeapp.generated.resources.icon_whats_app
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import theme.mainYellowColor
+import theme.onBackgroundDark
 import theme.onSecondaryContainerDark
 import theme.onSurfaceVariantDark
+import util.OpenGoogleMaps
 import util.OpenWhatsAppChat
 
 
@@ -52,6 +60,7 @@ fun ContactInfoScreen(onNavigateToProfile:() -> Unit) {
         TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
     val snackBarHostState = remember { SnackbarHostState() }
     var isOpenWhatsAppChat by remember { mutableStateOf(false) }
+    var isOpenGoogleMaps by remember { mutableStateOf(false) }
 
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
@@ -71,17 +80,29 @@ fun ContactInfoScreen(onNavigateToProfile:() -> Unit) {
                 .fillMaxSize(),
             contentPadding = innerPadding,
         ) {
-            subListHalfMouthContact{ isOpenWhatsAppChat = true }
+            subListHalfMouthContact(
+                onOpenWhatsAppChat = {isOpenWhatsAppChat = true },
+                onOpenGoogleMaps = {isOpenGoogleMaps = true}
+            )
         }
+
         if (isOpenWhatsAppChat) {
             OpenWhatsAppChat()
             isOpenWhatsAppChat = false
+        }
+
+        if (isOpenGoogleMaps) {
+            OpenGoogleMaps()
+            isOpenGoogleMaps = false
         }
     }
 }
 
 @OptIn(ExperimentalResourceApi::class)
-fun LazyListScope.subListHalfMouthContact(onOpenWhatsAppChat: () -> Unit) {
+fun LazyListScope.subListHalfMouthContact(
+    onOpenWhatsAppChat: () -> Unit,
+    onOpenGoogleMaps: () -> Unit
+    ) {
     items(1) {
         Text(
             modifier = Modifier.padding(top = 8.dp, start = 16.dp),
@@ -202,6 +223,24 @@ fun LazyListScope.subListHalfMouthContact(onOpenWhatsAppChat: () -> Unit) {
                 style = MaterialTheme.typography.titleMedium,
                 fontSize = 16.sp,
                 color = onSecondaryContainerDark,
+            )
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            val shape = RoundedCornerShape(10.dp)
+            Image(
+                painter = painterResource(Res.drawable.address),
+                contentDescription = null,
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(start = 24.dp, end = 24.dp, top = 16.dp, bottom = 16.dp)
+                    .border(1.dp, onBackgroundDark, shape)
+                    .clip(RoundedCornerShape(10.dp))
+                    .clickable { onOpenGoogleMaps() }
             )
         }
     }
