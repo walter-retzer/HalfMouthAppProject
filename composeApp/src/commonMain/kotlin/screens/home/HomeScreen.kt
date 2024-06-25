a369c2d2965dcbadec8c5e2a66237f0571406b55
walter <walter.d.retzer@gmail.com>
2024-06-24 22:17:18 -0300

    Ajuste do tempo de exibição da snackbar da notificação

--- composeApp/src/commonMain/kotlin/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/home/HomeScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
@@ -91,7 +92,8 @@ fun HomeScreen(
                     snackBarOnlyMessage(
                         snackBarHostState = snackBarHostState,
                         coroutineScope = scope,
-                        message = messageNotification
+                        message = messageNotification,
+                        duration = SnackbarDuration.Long
                     )
                 },
                 onNavigationProfileIconClick = { onNavigateToProfile() },
