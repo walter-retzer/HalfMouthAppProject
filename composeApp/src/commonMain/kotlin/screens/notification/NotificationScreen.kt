fb73b7d984bb5a3fe7051a870d78a8fa3ee8703f
walter <walter.d.retzer@gmail.com>
2024-06-24 21:46:43 -0300

    Ajuste do nome da toolbar

--- composeApp/src/commonMain/kotlin/screens/notification/NotificationScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/notification/NotificationScreen.kt
@@ -1,9 +1,7 @@
 package screens.notification
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
@@ -12,7 +10,7 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import components.profileToolbar
 import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
-import viewmodel.NotificationViewModel
+import viewmodel.HomeViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -22,7 +20,7 @@ fun NotificationScreen() {
     val viewModel = getViewModel(
         key = "notification-screen",
         factory = viewModelFactory {
-            NotificationViewModel()
+            HomeViewModel()
         }
     )
 
@@ -30,7 +28,7 @@ fun NotificationScreen() {
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             profileToolbar(
-                title = "Notification",
+                title = "Notificação",
                 onNavigationIconBack = {  },
                 onNavigationIconClose = {  },
                 scrollBehavior = scrollBehavior
@@ -38,9 +36,5 @@ fun NotificationScreen() {
         }
     ) {
 
-        Text(
-            modifier = Modifier.padding(it),
-            text = viewModel.response.toString()
-        )
     }
 }
