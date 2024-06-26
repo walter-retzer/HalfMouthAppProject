8cff16d135523bcd0edb42ca0dec29630e689c21
walter <walter.d.retzer@gmail.com>
2024-06-25 22:08:44 -0300

    Ajuste do método getInfoFirebaseRealTimeDatabase()

--- composeApp/src/commonMain/kotlin/viewmodel/HomeViewModel.kt
+++ composeApp/src/commonMain/kotlin/viewmodel/HomeViewModel.kt
@@ -7,19 +7,12 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
-import kotlinx.serialization.Serializable
+import util.ConstantsApp
 
 class HomeViewModel : ViewModel() {
 
     private val firebase = Firebase.database
-
-    private val _isNotificationActivated = MutableStateFlow(false)
-    val isNotificationActivated = _isNotificationActivated.asStateFlow()
-
-    private val _notificationDate = MutableStateFlow("")
-    val notificationDate = _notificationDate.asStateFlow()
-
-    private val _notificationMessage = MutableStateFlow("")
+    private val _notificationMessage = MutableStateFlow(ConstantsApp.MESSAGE_DEFAULT_NOTIFICATION)
     val notificationMessage = _notificationMessage.asStateFlow()
 
     init {
@@ -30,12 +23,8 @@ class HomeViewModel : ViewModel() {
         viewModelScope.launch {
             try {
                 val notification = firebase.reference("notifications").valueEvents.first()
-                val isActivated: Any = notification.child("list").child("isActivated").value ?: false
-                val date = notification.child("list").child("data").value ?: ""
-                val message = notification.child("list").child("message").value ?: ""
-
-                _isNotificationActivated.value = isActivated as Boolean
-                _notificationDate.value = date as String
+                val message = notification.child("list").child("message").value
+                    ?: ConstantsApp.MESSAGE_DEFAULT_NOTIFICATION
                 _notificationMessage.value = message as String
             } catch (e: Exception) {
                 println(" Errror $e")
@@ -43,10 +32,3 @@ class HomeViewModel : ViewModel() {
         }
     }
 }
-
-@Serializable
-data class RealTimeItemFirebase(
-    var data: String? = "",
-    var message: String? = "",
-    var index: String? = ""
-)
