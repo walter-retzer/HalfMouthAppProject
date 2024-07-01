70b3e58edbe304cb012bbc34f1947b38137ed3ed
walter <walter.d.retzer@gmail.com>
2024-07-01 19:17:25 -0300

    Ajuste da rota REQUEST_CHANNEL_FEED

--- composeApp/src/commonMain/kotlin/network/HttpRoutes.kt
+++ composeApp/src/commonMain/kotlin/network/HttpRoutes.kt
@@ -3,5 +3,5 @@ package network
 object HttpRoutes {
     private const val BASE_URL = "https://api.thingspeak.com"
     const val REQUEST_URL = "$BASE_URL/channels/2253688/feeds.json?"
-    const val REQUEST_CHANNEL_FEED = "$BASE_URL/channels/2253688/fields/5.json?"
+    const val REQUEST_CHANNEL_FEED = "$BASE_URL/channels/2253688/fields/"
 }
