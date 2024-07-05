118619d8c337cb8ad2caec3ee4c92eea273ed448
walter <walter.d.retzer@gmail.com>
2024-07-04 23:20:41 -0300

    Adicionado ChartLineViewModel ao appModule

--- composeApp/src/commonMain/kotlin/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/di/AppModule.kt
@@ -1,8 +1,10 @@
 package di
 
 import org.koin.dsl.module
+import viewmodel.ChartLineViewModel
 import viewmodel.HomeViewModel
 
 val appModule = module {
     single<HomeViewModel> { HomeViewModel() }
+    single<ChartLineViewModel> { ChartLineViewModel() }
 }
