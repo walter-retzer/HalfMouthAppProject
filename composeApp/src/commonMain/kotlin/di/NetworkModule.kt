b5eafb648a99423d417a5c5fea7468e7c9dc2d18
walter <walter.d.retzer@gmail.com>
2024-07-05 16:07:46 -0300

    Adicionado provideHttpClientModule

---
+++ composeApp/src/commonMain/kotlin/di/NetworkModule.kt
@@ -0,0 +1,24 @@
+package di
+
+
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.http.ContentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import org.koin.dsl.module
+
+val provideHttpClientModule = module {
+    factory {
+        HttpClient {
+            install(Logging) {
+                level = LogLevel.ALL
+            }
+            install(ContentNegotiation) {
+                json(json = Json { ignoreUnknownKeys = true }, contentType = ContentType.Any)
+            }
+        }
+    }
+}
