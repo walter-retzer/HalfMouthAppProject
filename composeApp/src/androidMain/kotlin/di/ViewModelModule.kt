5dd80643d393c01dc23da1fbf68bcc6ebeb183a4
walter <walter.d.retzer@gmail.com>
2024-07-04 23:20:10 -0300

    Adicionado ChartLineViewModel ao viewModelModule

--- composeApp/src/androidMain/kotlin/di/ViewModelModule.kt
+++ composeApp/src/androidMain/kotlin/di/ViewModelModule.kt
@@ -2,9 +2,11 @@ package di
 
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
+import viewmodel.ChartLineViewModel
 import viewmodel.HomeViewModel
 
 
 actual val viewModelModule = module{
     viewModelOf(::HomeViewModel)
+    viewModelOf(::ChartLineViewModel)
 }
--- composeApp/src/iosMain/kotlin/di/ViewModelModule.kt
+++ composeApp/src/iosMain/kotlin/di/ViewModelModule.kt
@@ -2,8 +2,10 @@ package di
 
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
+import viewmodel.ChartLineViewModel
 import viewmodel.HomeViewModel
 
 actual val viewModelModule = module{
     singleOf(::HomeViewModel)
+    singleOf(::ChartLineViewModel)
 }
