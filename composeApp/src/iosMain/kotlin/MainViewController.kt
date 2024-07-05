1820218ad0581093530c37180c061e8714a3167c
walter <walter.d.retzer@gmail.com>
2024-07-04 22:49:17 -0300

    Ajustado a inicialização do Koin no iOS

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,13 +1,16 @@
 import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
 import database.getTicketDataBase
+import di.KoinInitializer
 import navigation.NavHostMain
 
 
-fun MainViewController() = ComposeUIViewController {
-
+fun MainViewController() = ComposeUIViewController(
+    configure = {
+        KoinInitializer().init()
+    }
+) {
     val ticketDao = remember { getTicketDataBase().ticketDao() }
-
     NavHostMain(
         darkTheme = true,
         dynamicColor = false,
