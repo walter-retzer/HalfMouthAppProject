3edb20dad301da53a8327c48a1178601bdd9ea47
walter <walter.d.retzer@gmail.com>
2024-07-16 11:18:09 -0300

    Ajuste do valor da chave do CHANNEL_ID pelo arquivo secrets.properties

--- composeApp/src/commonMain/kotlin/network/HttpRoutes.kt
+++ composeApp/src/commonMain/kotlin/network/HttpRoutes.kt
@@ -1,7 +1,9 @@
 package network
 
+import secrets.BuildConfig
+
 object HttpRoutes {
     private const val BASE_URL = "https://api.thingspeak.com"
-    const val REQUEST_URL = "$BASE_URL/channels/2253688/feeds.json?"
-    const val REQUEST_CHANNEL_FEED = "$BASE_URL/channels/2253688/fields/"
+    const val REQUEST_URL = "$BASE_URL/channels/${BuildConfig.CHANNEL_ID}/feeds.json?"
+    const val REQUEST_CHANNEL_FEED = "$BASE_URL/channels/${BuildConfig.CHANNEL_ID}/fields/"
 }
