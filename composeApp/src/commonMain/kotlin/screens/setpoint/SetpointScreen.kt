5d83f4c2ea8ac69c55875fa82e76bcba2743a7f8
walter <walter.d.retzer@gmail.com>
2026-02-21 21:38:44 -0300

    Add composable SetpopintScreen

---
+++ composeApp/src/commonMain/kotlin/screens/setpoint/SetpointScreen.kt
@@ -0,0 +1,129 @@
+package screens.setpoint
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import components.DrawerMenuNavigation
+import components.MenuToolbar
+import components.MyAppCircularProgressIndicator
+import database.TicketDao
+import kotlinx.coroutines.launch
+import org.koin.compose.koinInject
+import util.snackBarOnlyMessage
+import viewmodel.SetpointAdjustViewModel
+import viewmodel.SetpointAdjustViewModelState
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SetpopintScreen(
+    ticketDao: TicketDao,
+    onNavigateToProfile: () -> Unit,
+    onNavigateFromDrawerMenu: (route: String) -> Unit,
+    viewModel: SetpointAdjustViewModel = koinInject()
+) {
+    val listOfTickets by ticketDao.getAllTickets().collectAsState(initial = emptyList())
+    val uiState by viewModel.uiState.collectAsState()
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
+    val snackBarHostState = remember { SnackbarHostState() }
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    var isSnackBarOpen by remember { mutableStateOf(false) }
+    var isSnackBarMessageErrorApiOpen by remember { mutableStateOf(false) }
+
+
+    ModalNavigationDrawer(
+        drawerContent = {
+            DrawerMenuNavigation(
+                scope = scope,
+                drawerState = drawerState,
+                tickets = listOfTickets.size,
+                onNavigateFromDrawerMenu = { route->
+                    onNavigateFromDrawerMenu(route)
+                }
+            )
+        },
+        drawerState = drawerState
+    ) {
+        Scaffold(
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            snackbarHost = { SnackbarHost(hostState = snackBarHostState) },
+            topBar = {
+                MenuToolbar(
+                    title = "Setpoint",
+                    onNavigationToMenu = {
+                        scope.launch {
+                            drawerState.open()
+                        }
+                    },
+                    onNavigationToProfile = { onNavigateToProfile() },
+                    onNavigateToNotifications = { },
+                    scrollBehavior = scrollBehavior
+                )
+            }
+        ) { innerPadding ->
+            when (val state = uiState) {
+
+                is SetpointAdjustViewModelState.Error -> {
+                   if(!isSnackBarMessageErrorApiOpen) {
+                       snackBarOnlyMessage(
+                           snackBarHostState = snackBarHostState,
+                           coroutineScope = scope,
+                           message = state.message
+                       )
+                       isSnackBarMessageErrorApiOpen = true
+                   }
+                }
+
+                is SetpointAdjustViewModelState.Loading -> {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        MyAppCircularProgressIndicator()
+                    }
+                }
+
+                is SetpointAdjustViewModelState.ErrorNetworkConnection -> {
+                    if(!isSnackBarOpen) {
+                        snackBarOnlyMessage(
+                            snackBarHostState = snackBarHostState,
+                            coroutineScope = scope,
+                            message = state.message
+                        )
+                        isSnackBarOpen = true
+                    }
+                }
+
+                is SetpointAdjustViewModelState.SuccessUpdateValues -> {
+                    if(!isSnackBarOpen) {
+                        snackBarOnlyMessage(
+                            snackBarHostState = snackBarHostState,
+                            coroutineScope = scope,
+                            message = state.value.toString()
+                        )
+                        isSnackBarOpen = true
+                    }
+                }
+            }
+        }
+    }
+}
