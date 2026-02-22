58c150fee214a97f1a2daa8bc7042481c06fe6d2
walter <walter.d.retzer@gmail.com>
2026-02-21 21:07:02 -0300

    Add end point do request de update values

--- composeApp/src/commonMain/kotlin/network/HttpRoutes.kt
+++ composeApp/src/commonMain/kotlin/network/HttpRoutes.kt
@@ -6,4 +6,5 @@ object HttpRoutes {
     private const val BASE_URL = "https://api.thingspeak.com"
     const val REQUEST_URL = "$BASE_URL/channels/${BuildConfig.CHANNEL_ID}/feeds.json?"
     const val REQUEST_CHANNEL_FEED = "$BASE_URL/channels/${BuildConfig.CHANNEL_ID}/fields/"
+    const val REQUEST_CHANNEL_UPDATE_FIELD = "$BASE_URL/update"
 }
