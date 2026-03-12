a4ecd3eb57d46b642f5b4ed8626ae8201eaf2c02
walter <walter.d.retzer@gmail.com>
2026-03-11 23:02:49 -0300

    ajustado fonts do Typography

--- composeApp/src/commonMain/kotlin/theme/Type.kt
+++ composeApp/src/commonMain/kotlin/theme/Type.kt
@@ -1,10 +1,52 @@
 package theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import halfmouthappproject.composeapp.generated.resources.IndieFlower_Regular
+import halfmouthappproject.composeapp.generated.resources.Poppins_Regular
+import halfmouthappproject.composeapp.generated.resources.Res
+import halfmouthappproject.composeapp.generated.resources.Roboto_Bold
+import halfmouthappproject.composeapp.generated.resources.Roboto_Medium
+import halfmouthappproject.composeapp.generated.resources.Roboto_Regular
+import org.jetbrains.compose.resources.Font
+
+@Composable
+fun appFontFamily(): FontFamily {
+    return FontFamily(
+        //Font(Res.font.Poppins_Regular, weight = FontWeight.Normal),
+        Font(Res.font.Roboto_Medium, weight = FontWeight.Medium),
+        //Font(Res.font.Roboto_Bold, weight = FontWeight.Bold),
+        //Font(Res.font.Roboto_Regular, weight = FontWeight.Normal),
+    )
+}
+
+@Composable
+fun AppTypography(): Typography {
+    val fontFamily = appFontFamily()
+
+    return Typography().run {
+        copy(
+            bodyLarge = bodyLarge.copy(fontFamily = fontFamily),
+            bodyMedium = bodyMedium.copy(fontFamily = fontFamily),
+            bodySmall = bodySmall.copy(fontFamily = fontFamily),
+            titleLarge = titleLarge.copy(fontFamily = fontFamily),
+            titleMedium = titleMedium.copy(fontFamily = fontFamily),
+            titleSmall = titleSmall.copy(fontFamily = fontFamily),
+            labelLarge = labelLarge.copy(fontFamily = fontFamily),
+            labelMedium = labelMedium.copy(fontFamily = fontFamily),
+            labelSmall = labelSmall.copy(fontFamily = fontFamily),
+            headlineLarge = headlineLarge.copy(fontFamily = fontFamily),
+            headlineMedium = headlineMedium.copy(fontFamily = fontFamily),
+            headlineSmall = headlineSmall.copy(fontFamily = fontFamily),
+        )
+    }
+}
+
+
 
 val typography = Typography(
     bodyLarge = TextStyle(
